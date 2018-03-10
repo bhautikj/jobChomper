0079947393cd9b54f9710a263da1fb2ea70422d8
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-10 14:06:42 -0800

    tuning

--- jobFlinger/node.py
+++ jobFlinger/node.py
@@ -8,7 +8,13 @@ class Node(object):
   def run(self):
     raise ValueError("[Node] can't run node base class")
     
+class StartNode(Node):
+  def __init__(self):
+    super().__init__()
 
+  def run(self):
+    return True
+      
 def nodeExists(nodeName):
   for clss in Node.__subclasses__():
     if clss.__name__ == nodeName:
