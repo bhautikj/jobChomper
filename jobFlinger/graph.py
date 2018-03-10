d08efe60a70a95dab6c558dacc81cba65533fa7a
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-09 22:06:24 -0800

    graph reading init

--- jobFlinger/graph.py
+++ jobFlinger/graph.py
@@ -1,4 +1,55 @@
+## 
+## Weirdo DAG that powers jobFlinger
+## --
+##
+## Assertions: 
+##   * DAG is made up of named edges
+##   * Each edge is a triple (A, B, NEEDSPREVIOUSTOPASS)
+##     A, B are the named nodes
+##     B will execute after A has evaluated
+##     NEEDSPREVIOUSTOPASS is True or False; if it is True then A _must_ evaluate as True for B to run
+##   * There's a special node called STARTNODE from where execution starts
+##   * Comment lines in graph file start with #
+##   * Elements in graph lines separated by ',' - for example:
+##     A, B, True
+##
+
 class Graph(object):
   """ Graph Object """
+  edges = set()
+  init = False
+  
   def __init__(self):
     self.init = True
+    
+    
+  def loadGraphFromFile(self, filename):
+    with open(filename) as graphBody:
+      data = graphBody.read()
+      for line in data.split('\n'):
+        line = line.strip()
+        # Empty line
+        if line == '':
+          continue
+          
+        # Comment line
+        if line[0] == '#':
+          continue
+        spl = line.split(',')
+        
+        # Not a triple
+        if len(spl) != 3:
+          raise ValueError("Problem parsing: " + filename + " file has invalid triple: " + line)
+        
+        nodeA = spl[0]
+        nodeB = spl[1]
+        prevEval = False
+        if spl[2].lower() == 'true':
+          prevEval = True
+      
+        triple = (nodeA, nodeB, prevEval)
+        
+        self.edges.add(triple)
+      
+      
+    return True
--- tests/testGraph.py
+++ tests/testGraph.py
@@ -1,11 +1,28 @@
+import os
+testdir = os.path.dirname(__file__)
 import unittest
 
 import jobFlinger.graph
 
-class TestLoadGraph(unittest.TestCase):
+
+class TestGraph(unittest.TestCase):
   def test_init(self):
     x = jobFlinger.graph.Graph()
     self.assertTrue(x.init)
+
+  def test_read_nofile(self):
+    x = jobFlinger.graph.Graph()
+    self.assertRaises(FileNotFoundError,  x.loadGraphFromFile, "BLAH")
+
+  def test_read_borkedFile(self):
+    x = jobFlinger.graph.Graph()
+    self.assertRaises(ValueError, x.loadGraphFromFile, os.path.join(testdir,"borkedGraph.graph"))
     
+  def test_read_validFile(self):
+    x = jobFlinger.graph.Graph()
+    readPass = x.loadGraphFromFile(os.path.join(testdir,"validGraph.graph"))
+    self.assertTrue(readPass)
+
+  
 if __name__ == '__main__':
   unittest.main()
