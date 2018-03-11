4fef980e86aeec82d0455d59aa3142ceccbcac9f
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-11 10:52:01 -0700

    simplified

--- jobFlinger/safeFileDict.py
+++ jobFlinger/safeFileDict.py
@@ -1,11 +1,40 @@
 import threading
+import json
 
 # see https://stackoverflow.com/a/2390997
 class SafeFileDict(dict):  # dicts take a mapping or iterable as their optional first argument
   def __init__(self, *args, **kwargs):
     self.lock = threading.Lock()
     self.update(*args, **kwargs)
+    self.doJournal = False
+    self.journalFile = ""
 
+  def enableJournal(self, journalFile):
+     self.journalFile = journalFile
+     self.doJournal = True
+
+  def writeJournal(self):
+    if self.doJournal == False:
+      return
+
+    with self.lock:
+      data = json.dumps (self, indent=2)
+      with open(self.journalFile, 'w') as file:
+        file.write(data)
+
+  def readJournal(self, journalFile=""):
+    with self.lock:
+      if journalFile == "" && self.doJournal == False:
+        return
+      
+      if journalFile != "":
+        toRead = journalFile
+      else
+        toRead = self.journalFile
+      
+      with open(toRead, 'r') as file:
+        self = json.loads(file.read())
+        
   def __getitem__(self, key):
     with self.lock:
       val = dict.__getitem__(self, key)
--- tests/testSafeFileDict.py
+++ tests/testSafeFileDict.py
@@ -3,3 +3,11 @@ testdir = os.path.dirname(__file__)
 import unittest
 
 import jobFlinger.safeFileDict
+
+
+class TestSafeFileDict(unittest.TestCase):  
+      
+  def test_dict_exercise(self):
+    pass
+    #nodeFail = jobFlinger.node.Node()
+    #self.assertRaises(ValueError,  nodeFail.run)
