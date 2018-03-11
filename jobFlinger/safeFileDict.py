2938ad067dbe2da3b31def8cc55d8d109e926544
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-11 00:20:56 -0800

    simplified

--- jobFlinger/safeFileDict.py
+++ jobFlinger/safeFileDict.py
@@ -1,56 +1,29 @@
-#
-# inspired by/cribbed from
-# https://stackoverflow.com/questions/3387691/how-to-perfectly-override-a-dict
-#
-
-from itertools import chain
-try:              # Python 2
-    str_base = basestring
-    items = 'iteritems'
-except NameError: # Python 3
-    str_base = str, bytes, bytearray
-    items = 'items'
-
 import threading
 
-_RaiseKeyError = object() # singleton for no-default behavior
-
+# see https://stackoverflow.com/a/2390997
 class SafeFileDict(dict):  # dicts take a mapping or iterable as their optional first argument
-  __slots__ = () # no __dict__ - that would be redundant
-  lock = threading.Lock()
-  @staticmethod # because this doesn't make sense as a global function.
-  def _process_args(mapping=(), **kwargs):
-    if hasattr(mapping, items):
-      mapping = getattr(mapping, items)()
-    return ((k, v) for k, v in chain(mapping, getattr(kwargs, items)()))
-  def __init__(self, mapping=(), **kwargs):
-    super(SafeFileDict, self).__init__(self._process_args(mapping, **kwargs))
-  def __getitem__(self, k):
+  def __init__(self, *args, **kwargs):
+    self.lock = threading.Lock()
+    self.update(*args, **kwargs)
+
+  def __getitem__(self, key):
     with self.lock:
-      item = super(SafeFileDict, self).__getitem__(k)
-      return item
-  def __setitem__(self, k, v):
+      val = dict.__getitem__(self, key)
+      print ('GET', key)
+      return val
+
+  def __setitem__(self, key, val):
     with self.lock:
-      item = super(SafeFileDict, self).__setitem__(k, v)
-      return item
-  def __delitem__(self, k):
-    return super(SafeFileDict, self).__delitem__(k)
-  def get(self, k, default=None):
-    return super(SafeFileDict, self).get(k, default)
-  def setdefault(self, k, default=None):
-    return super(SafeFileDict, self).setdefault(k, default)
-  def pop(self, k, v=_RaiseKeyError):
-    if v is _RaiseKeyError:
-      return super(SafeFileDict, self).pop(k)
-    return super(SafeFileDict, self).pop(k, v)
-  def update(self, mapping=(), **kwargs):
-    super(SafeFileDict, self).update(self._process_args(mapping, **kwargs))
-  def __contains__(self, k):
-    return super(SafeFileDict, self).__contains__(k)
-  def copy(self): # don't delegate w/ super - dict.copy() -> dict :(
-    return type(self)(self)
-  @classmethod
-  def fromkeys(cls, keys, v=None):
-    return super(SafeFileDict, cls).fromkeys((k for k in keys), v)
+      print ('SET', key, val)
+      dict.__setitem__(self, key, val)
+
   def __repr__(self):
-    return '{0}({1})'.format(type(self).__name__, super(SafeFileDict, self).__repr__())
+    with self.lock:
+      dictrepr = dict.__repr__(self)
+      return '%s(%s)' % (type(self).__name__, dictrepr)
+
+  def update(self, *args, **kwargs):
+    with self.lock:
+      print ('update', args, kwargs)
+      for k, v in dict(*args, **kwargs).items():
+        self[k] = v
