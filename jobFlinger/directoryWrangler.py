d98dee0eba6d8e42bd7edff043276846c9148245
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-11 19:50:54 -0700

    separating out directory wrangler

---
+++ jobFlinger/directoryWrangler.py
@@ -0,0 +1,48 @@
+import os, uuid
+
+import jobFlinger.runGraph
+
+#
+# For jobFlinger we need:
+#  A) a var dir - jobs in progress live here and can survive application failure
+#  B) a tmp dir - somewhere to store tmp data; ephemeral data that can be lost
+#  C) a done dir - somewhere to move jobs after they are done
+#
+# * Each job gets a UUID
+# * Within var and tmp we generate a directory with the UUID of the job
+#
+class DirectoryWrangler(object):
+  """ DirectoryWrangler Object """
+  def __init__(self, varDir, tmpDir, doneDir):
+    # store as absolute paths - these will always work if passed
+    # to external calls, whereas relative paths are a crapshoot
+    self.varDir = os.path.abspath(varDir)
+    self.tmpDir = os.path.abspath(tmpDir)
+    self.doneDir = os.path.abspath(doneDir)
+  
+  def createJob(self):
+    jobID = str(uuid.uuid4())
+    jobDir = os.path.join(self.varDir, jobID)
+    os.mkdir(jobDir)
+    return jobID
+    
+  def getJobVar(self, jobID):
+    return os.path.join(self.varDir, jobID)
+
+  def getJobTmp(self, jobID):
+    return os.path.join(self.tmpDir, jobID)
+
+  def getJobDone(self, jobID):
+    return os.path.join(self.doneDir, jobID)
+ 
+  def getActiveJobs(self):
+    # just list directories in var
+    subfolders = [f.path for f in os.scandir(self.varDir) if f.is_dir()]
+
+    # only get the dirs with JOBSTATEFILE
+    subfolders = [f for f in subfolders if os.path.exists(os.path.join(f, jobFlinger.runGraph.JOBSTATEFILE))]
+
+    # remove dot files
+    subfolders = [os.path.basename(f) for f in subfolders if f[0] != '.']
+    
+    return subfolders
---
+++ tests/testDirectoryWrangler.py
@@ -0,0 +1,40 @@
+import os
+testdir = os.path.dirname(__file__)
+import unittest
+import shutil
+import uuid
+
+import jobFlinger.directoryWrangler
+import jobFlinger.runGraph
+
+def touch(fname):
+    open(fname, 'a').close()
+    os.utime(fname, None)
+
+class TestDirectoryWrangler(unittest.TestCase):
+  def test_init(self):
+    testBase = os.path.join(testdir, str(uuid.uuid4()))
+    os.mkdir(testBase)
+    
+    testTmp = os.path.join(testBase, "tmp")
+    os.mkdir(testTmp)
+    testVar = os.path.join(testBase, "var")
+    os.mkdir(testVar)
+    testDone = os.path.join(testBase, "done")
+    os.mkdir(testDone)
+    
+    
+    try:
+      w = jobFlinger.directoryWrangler.DirectoryWrangler(testVar, testTmp, testDone)
+      jobA = w.createJob()
+      self.assertTrue(os.path.isdir(os.path.join(testVar, jobA)))
+      touch(os.path.join(testVar, jobA, jobFlinger.runGraph.JOBSTATEFILE))
+      jobB = w.createJob()
+      self.assertTrue(os.path.isdir(os.path.join(testVar, jobB)))
+      touch(os.path.join(testVar, jobB, jobFlinger.runGraph.JOBSTATEFILE))
+      
+      jobs = set(w.getActiveJobs())
+      ejobs = set([jobA, jobB])
+      self.assertTrue(jobs == ejobs)      
+    finally:
+      shutil.rmtree(testBase)
