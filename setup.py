ea108e1969f38debc8fb3cea22151ba2b3010bac
Bhautik Joshi <bjoshi+github@gmail.com>
2018-03-14 20:23:21 -0700

    packaging

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="jobChomper",
+
+    # Version number (initial):
+    version="0.1.0",
+
+    # Application author details:
+    author="Bhautik Joshi",
+    author_email="bjoshi@gmail.com",
+
+    # Packages
+    packages=["jobChomper"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/bhautikj/jobChomper",
+
+    #
+    license="LICENSE",
+    description="Robustly queue & run a large number of jobs in parallel",
+
+    # long_description=open("README.txt").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[],
+    
+    python_requires='>=3.2',
+)
